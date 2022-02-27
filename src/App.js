0f776bf0b76d3447c107d674ff36f44dd28aabfa
mohammedsaif001 <mdsaif4online@gmail.com>
2022-02-27 14:00:00 +0530

    Completing Home Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM Convention
-    <div className="app">
-      {/* Header */}
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <Home />
+          </Route>
+          <Route exact path="/checkout">
+            <Header />
+            <h1>THIS IS CHECKOUT PAGE</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
