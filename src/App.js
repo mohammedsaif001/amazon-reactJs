94994cac5066d28e583a98a182a1c6ff15feee5d
mohammedsaif001 <mdsaif4online@gmail.com>
2022-02-28 19:55:38 +0530

    Adding Checkout Page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Components/Checkout";
 
 function App() {
   return (
     // BEM Convention
     <Router>
       <div className="app">
+        <Header />
         <Switch>
           <Route exact path="/">
-            <Header />
             <Home />
           </Route>
           <Route exact path="/checkout">
-            <Header />
-            <h1>THIS IS CHECKOUT PAGE</h1>
+            <Checkout />
           </Route>
         </Switch>
       </div>
