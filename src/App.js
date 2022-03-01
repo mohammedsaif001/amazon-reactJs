cf95d72276f76222c066f27f07a39bf175904e22
mohammedsaif001 <mdsaif4online@gmail.com>
2022-03-01 19:03:10 +0530

    Creating LoginPage

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Header from "./Components/Header";
 import Home from "./Components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Components/Checkout";
+import Login from "./Components/Login";
 
 function App() {
   return (
     // BEM Convention
     <Router>
       <div className="app">
-        <Header />
         <Switch>
           <Route exact path="/">
+            <Header />
             <Home />
           </Route>
           <Route exact path="/checkout">
+            <Header />
             <Checkout />
+          </Route>
+          <Route exact path="/login">
+            <Login />
           </Route>
         </Switch>
       </div>
---
+++ src/Components/Login.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+
+function Login() {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault(); //Prevents from refreshing
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt="Amazon Logo"
+        />
+      </Link>
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setpassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="login__signInButton"
+            onClick={signIn}
+          >
+            Sign In
+          </button>
+        </form>
+        <p>
+          By Signing-in you agree to Amazon's conditions of use &amp;Sale.
+          Please see our Privacy Notice, our Cookies Notice &amp; our
+          Interst-Based ads
+        </p>
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
